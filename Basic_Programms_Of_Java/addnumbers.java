9fc1d423b1e79570a3927f40ae48ee2af4f649ad
het2809 <hetk93@gmail.com>
2023-07-31 18:16:31 +0530

    added sum and divison program using java

---
+++ Basic_Programms_Of_Java/addnumbers.java
@@ -0,0 +1,10 @@
+public class addnumbers {
+    public static void main(String[] args) {
+      
+        int a = 74;
+        int b = 36;
+        int c = a+b;
+        System.out.println("The Sum Is :" + c);
+        
+    }    
+}
