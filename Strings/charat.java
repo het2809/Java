30ea5d2007b68857ab5c6e31bc28e5c1d5f7bf05
het2809 <hetk93@gmail.com>
2023-08-03 17:08:21 +0530

    added character-at program

---
+++ Strings/charat.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class charat {
+    public static void main(String[] args) {
+        System.out.println("Enter String");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        int position = sc.nextInt();
+        System.out.println(userInput.charAt(position));
+        main1(args);
+    }
+private static void main1(String[] args) {
+        System.out.println("Enter String");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        int position = sc.nextInt();
+        System.out.println(userInput.charAt(position));
+        
+    }
+
+}
