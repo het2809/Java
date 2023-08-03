4b9b7a61820caeb6d4dc04141744480b82cfb2c5
het2809 <hetk93@gmail.com>
2023-08-03 10:32:32 +0530

    edited trim program

--- Strings/trim.java
+++ Strings/trim.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class trim {
     public static void main(String[] args) {
-        System.out.printf("WHAT IS YOUR String ");
+        System.out.printf("ENTER YOUR STRING THAT YOU WANT TO TRIM  ");
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        String trim = name.trim();
+        String string = sc.nextLine();
+        String trim = string.trim();
         System.out.println(trim);
     }
 }
