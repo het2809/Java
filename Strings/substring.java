d83a398d50ef4ca2e41a8ec485c52d1fc789232d
het2809 <hetk93@gmail.com>
2023-08-03 11:08:28 +0530

    edited substring program

--- Strings/substring.java
+++ Strings/substring.java
@@ -3,8 +3,8 @@ public class substring {
     public static void main(String[] args) {
         System.out.println("Enter String");
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        String substring = name.substring(3 , 8);
+        String string = sc.nextLine();
+        String substring = string.substring(3 , 8);
         System.out.println(substring);
     }
 }
