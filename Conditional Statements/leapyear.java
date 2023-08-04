92cbf194c85f3498d3509e9f9ae026e768e1570a
het2809 <hetk93@gmail.com>
2023-08-04 10:37:39 +0530

    added program that checks year is leap year or not leap year

--- Conditional Statements/leapyear.java
+++ Conditional Statements/leapyear.java
@@ -5,7 +5,7 @@ public class leapyear {
         Scanner sc = new Scanner(System.in);
         System.out.print("ENTER YEAR : ");
         int year = sc.nextInt();
-        if (year % 4 == 0 || year % 100 != 0 && year % 400 == 0)  {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)  {
             System.out.println("LEAP YEAR");
         } else {
             System.out.println("NOT LEAP YEAR");           
