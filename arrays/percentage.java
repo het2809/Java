2d73eef65827b12d74931a51f81ed7c0e1086c05
het2809 <hetk93@gmail.com>
2023-08-11 11:41:45 +0530

    added program that calculates perecentage of 7 subjects

--- arrays/percentage.java
+++ arrays/percentage.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo {
+public class percentage {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         float[] marks = new float[7];
