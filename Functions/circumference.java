e0425b83a62ca4c02c8e76fa731d029605fcb705
het2809 <hetk93@gmail.com>
2023-08-16 13:02:07 +0530

    edited program of circumference

--- Functions/circumference.java
+++ Functions/circumference.java
@@ -1,21 +1,13 @@
 import java.util.Scanner;
 
 public class circumference {
-    public static void circumference(float radius) {
-        System.out.print("Enter radius for circumference function : ");
-        Scanner sc = new Scanner(System.in);
-        float rad = sc.nextFloat();
-        float pie = 3.14f;
-        float circle = 2 * pie * rad;
-        System.out.print("circumference of circle is : ");
-        System.out.print(circle);
+    public static float getCircumference(float radius) {
+        return 2 * 3.14f * radius;
     }
 
-    public static void main(String[] args) {
-        System.out.print("Enter radius for main function : ");
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        float rad = sc.nextFloat();
-        circumference(rad);
-
+        float r = sc.nextFloat();
+        System.out.println(getCircumference(r));
     }
 }
