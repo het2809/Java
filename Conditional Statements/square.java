87ed447e640568376e805a85a30ec505f62ff029
het2809 <hetk93@gmail.com>
2023-08-17 11:37:32 +0530

    added some programs of conditional statement

--- Conditional Statements/square.java
+++ Conditional Statements/square.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo12 {
+public class square {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Length : ");
--- Conditional Statements/validtriangle.java
+++ Conditional Statements/validtriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo13 {
+public class validtriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the first angle: ");
