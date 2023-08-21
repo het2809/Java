2b609d3f8c681eb1f1008d8389d46f44f18860f5
het2809 <hetk93@gmail.com>
2023-08-21 10:50:05 +0530

    added program that sums of array

---
+++ arrays/sumArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sumArray {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+        System.out.println("Enter Elements : ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Your Elements Are : ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+            sum = arr[i] + sum;
+
+        }
+        System.out.println();
+        System.out.println("Sum of array is : " + sum);
+    }
+
+}
