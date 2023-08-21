cd7165f54c036fcfee7c89308f8ddb876518f5aa
het2809 <hetk93@gmail.com>
2023-08-21 11:57:52 +0530

    added program that convert array to descending order

---
+++ arrays/descending.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class descending {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size Of Array : ");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter Your Element Of Array : ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.print("Ascending order is : ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
