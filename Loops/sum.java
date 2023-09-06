9aeb3386955fc21b616daef430ead597f01c3072
het2809 <hetk93@gmail.com>
2023-09-06 18:11:38 +0530

    edited sum program

--- Loops/sum.java
+++ Loops/sum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class summm {
+public class sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter N : ");
