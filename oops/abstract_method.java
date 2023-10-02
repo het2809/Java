90c1a6e9a2d8662918de83b79a043ac9c4b6e5be
het2809 <hetk93@gmail.com>
2023-10-02 16:37:07 +0530

    added program of abstract

---
+++ oops/abstract_method.java
@@ -0,0 +1,24 @@
+abstract class Animals {
+    abstract void walk();
+}
+
+class Horse {
+    public void walk() {
+        System.out.println("Horse Is Walking With 4 Legs");
+    }
+}
+
+class Chiken {
+    public void walk() {
+        System.out.println("Chiken Is Walking With 2 Legs");
+
+    }
+}
+
+public class abstract_method {
+    public static void main(String[] args) {
+        Horse h1 = new Horse();
+        h1.walk();
+        Animals animals = new Animals();
+    }
+}
