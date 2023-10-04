14fb6d38fe8dc605904dc5b070584b2e07616806
het2809 <hetk93@gmail.com>
2023-10-04 18:24:05 +0530

    added basic program of ArrayList

---
+++ Arraylist/add.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class add {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(0);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+    }
+}
