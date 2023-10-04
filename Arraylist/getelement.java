d5ba950e657589cd24c28cf559e215c60b036551
het2809 <hetk93@gmail.com>
2023-10-04 18:36:33 +0530

    added program that gets element from ArrayList

---
+++ Arraylist/getelement.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class getelement {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(0);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+
+        int element = list.get(0);
+        System.out.println(element);
+    }
+}
