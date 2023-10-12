04675e739ffb42894ece366b6c76da73fe2f2071
het2809 <hetk93@gmail.com>
2023-10-12 10:22:56 +0530

    added program that prints linked list

---
+++ Linked List/printLl2.java
@@ -0,0 +1,29 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class demo {
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(8);
+        Node d = new Node(9);
+        Node e = new Node(16);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        Node temp = a;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+}
