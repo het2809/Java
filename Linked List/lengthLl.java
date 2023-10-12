c24fc605cfc4a607107427c75a42b07e1c110c67
het2809 <hetk93@gmail.com>
2023-10-12 11:36:43 +0530

    added program that calculates length of Linked List

---
+++ Linked List/lengthLl.java
@@ -0,0 +1,42 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class lengthLl {
+    public static void displayr(Node head) {
+        if (head == null) {
+            return;
+        }
+        displayr(head.next);
+        System.out.print(head.data + " -> ");
+    }
+
+    public static int length(Node head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(8);
+        Node d = new Node(9);
+        Node e = new Node(16);
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        System.out.print("Length Is : " + length(a));
+
+    }
+}
